f3316fb22b1ce1307d86ec84f174906879135d4b
Eunmi Lee <eunoia0235@gmail.com>
2025-06-07 03:18:27 +0900

    refactor: 라우터 엔드포인트 '/api'로 변경

--- app.ts
+++ app.ts
@@ -1,19 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import routes from './routes';  // routes/index.ts import
+import routes from './routes';
 
-// 환경변수 설정
 dotenv.config();
 
 const app = express();
 
-// 미들웨어 설정
 app.use(cors());
 app.use(express.json());
 
-console.log(' app.ts 진입');
-// 라우트 연결 (예: routes/index.ts)
- app.use('/', routes);
+app.use('/api', routes);
 
 export default app;
